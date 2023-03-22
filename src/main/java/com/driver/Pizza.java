e009655dae2456f9605d180b37d91d0ca3b553ee
Tamilselvi459 <tamilselvig459@gmail.com>
2023-03-22 20:19:35 +0530

    Assingment done

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,18 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)
+        {
+             price = 300;
+            System.out.println("Base Price Of The Pizza: 300"); }
+        else {
+            price = 400;
+            System.out.println("Base Price Of The Pizza: 400");
+        }
     }
 
     public int getPrice(){
@@ -17,18 +26,33 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        price += 80;
+        System.out.println("Extra Cheese Added: 80");
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg) {
+            price += 70;
+            System.out.println("Extra Toppings Added: 70");
+        }
+        else {
+            price += 120;
+            System.out.println("Extra Toppings Added: 120");
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        price += 20;
+        System.out.println("Paperbag Added: 20");
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+//        return "Total Price: "+price;
+//        bill = String.valueOf(price);
+        bill  = "Total Price: "+price+"\n";
+      return this.bill;
     }
 }
